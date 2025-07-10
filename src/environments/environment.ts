9eb81172915906181714cadca9df0ef09e6434f8
JC <Jeanclaude.lequigan.22@usjr.edu.ph>
2025-07-10 11:51:42 +0800

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyDBowIhypiRQeeMay8yOTGbKZ3aATnHF-c",
-    authDomain: "guardiancare-42d0e.firebaseapp.com",
-    projectId: "guardiancare-42d0e",
-    storageBucket: "guardiancare-42d0e.appspot.com",
-    messagingSenderId: "245676224765",
-    appId: "1:245676224765:web:194288d285426cd7020e94",
-    measurementId: "G-57REVD6BSB"
+    apiKey: "AIzaSyCvJ4fD4TixuBx1901grgsZw7W8tGm27SQ",
+  authDomain: "guardiancare-eba41.firebaseapp.com",
+  projectId: "guardiancare-eba41",
+  storageBucket: "guardiancare-eba41.firebasestorage.app",
+  messagingSenderId: "86064840944",
+  appId: "1:86064840944:web:4c1c1bdd34e895ef15c235"
   }
 };
