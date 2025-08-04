d6276d67db323de9144a6a16c38acda96b75c6ec
JC <Jeanclaude.lequigan.22@usjr.edu.ph>
2025-08-04 17:25:33 +0800

    added the android newly fixes again smaller bugs need more optimized

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,20 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'guardian-care-app',
-  webDir: 'www'
+  appId: 'com.guardiancare.app',
+  appName: 'GuardianCare',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  },
+  android: {
+    buildOptions: {
+      keystorePath: undefined,
+      keystoreAlias: undefined,
+      keystorePassword: undefined,
+      keyPassword: undefined
+    }
+  }
 };
 
 export default config;
